95e64071a9bf31ca493cf50e6e4ca968991eac8d
JT <62544370+RSTsai@users.noreply.github.com>
2021-05-04 15:45:53 +0800

    Update VideoToPicture.py

--- VideoToPicture.py
+++ VideoToPicture.py
@@ -2,39 +2,40 @@ import cv2
 from os import makedirs, path
 
 
+
 # 影片檔名
-videoName = '153749'
+VideoName = '153749'
 # 影片全路徑
-videoPath = f'./VideoToPicture/20210325/videoCam0/{videoName}.avi' 
+VideoPath = f'./VideoToPicture/20210325/videoCam0/{VideoName}.avi' 
 # 取樣幀數間隔
-time_F = 150 
+FrameDelta = 150 
 # 圖像resize尺寸
-size_w = 640
-size_h = 480
+SizeW = 640
+SizeH = 480
 
 
 # 存檔資料夾
-saveFolder = './VideoToPicture/JPEGImages/'
+SaveFolder = './VideoToPicture/JPEGImages/'
 # 存檔名稱開頭
-saveFileNameHeader = f'NN_{videoName}_'
+SaveFileNameHeader = f'NN_{VideoName}_'
 # 存檔名稱起始編號
-saveNumStart = 0
+SaveNumStart = 0
 # 存檔名稱數字位數 (zfill)
-zfillNum = 6 
+ZfillNum = 6 
 
 
 
-def get_images_from_video(videoPath, time_F):
+def GetImagesFromVideo(VideoPath, deltaF):
     frameNum = 0
     saveNum = 0
     video_images = []
 
     # 確認存檔資料夾存在
-    if CheckDir(saveFolder) == False:
+    if CheckDir(SaveFolder) == False:
         return
 
     # 判斷是否開啟影片
-    vc = cv2.VideoCapture(videoPath) 
+    vc = cv2.VideoCapture(VideoPath) 
     if vc.isOpened():
         rval = True
     else:
@@ -52,15 +53,15 @@ def get_images_from_video(videoPath, time_F):
             continue
         
         # 每隔幾幀進行擷取
-        if(frameNum % time_F == 0): 
+        if(frameNum % deltaF == 0): 
             # 每保存一張saveNum+1
             saveNum = saveNum + 1 
             # 存檔檔名
-            saveFileName = saveFolder + saveFileNameHeader + str(saveNumStart + saveNum).zfill(zfillNum)+ '.jpg'
+            saveFileName = SaveFolder + SaveFileNameHeader + str(SaveNumStart + saveNum).zfill(ZfillNum)+ '.jpg'
             # 圖像格式化
-            video_frame_format = cv2.resize(video_frame, (size_w,size_h))
+            video_frame_format = cv2.resize(video_frame, (SizeW,SizeH))
             # 保存圖像
-            save_image(video_frame_format, saveFileName)
+            saveImage(video_frame_format, saveFileName)
             video_images.append(video_frame_format)   
             # 顯示圖像
             # cv2.imshow('windows', video_images[i])
@@ -71,10 +72,10 @@ def get_images_from_video(videoPath, time_F):
     return video_images
 
 
-def save_image(image,saveFilePath):
+def saveImage(image,saveFilePath):
     print('save image: ',saveFilePath)
     cv2.imwrite(saveFilePath,image)
- 
+
  
 def CheckDir(dirPath):
     try:
@@ -97,5 +98,5 @@ def CheckDir(dirPath):
 
 if __name__ =="__main__":
     # 讀取影片並轉成圖片
-    video_images = get_images_from_video(videoPath, time_F) 
+    video_images = GetImagesFromVideo(VideoPath, FrameDelta) 
 
